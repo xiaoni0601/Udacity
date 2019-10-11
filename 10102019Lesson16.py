e40f1cbc4fc3da073164de42c88f03815defdefa
xiaoni0601 <53635574+xiaoni0601@users.noreply.github.com>
2019-10-11 11:04:09 +0800

    Lesson16

---
+++ 10102019Lesson16.py
@@ -0,0 +1,355 @@
+# 1 Gold Star
+
+# The built-in <string>.split() procedure works
+# okay, but fails to find all the words on a page
+# because it only uses whitespace to split the
+# string. To do better, we should also use punctuation
+# marks to split the page into words.
+
+# Define a procedure, split_string, that takes two
+# inputs: the string to split and a string containing
+# all of the characters considered separators. The
+# procedure should return a list of strings that break
+# the source string up by the characters in the
+# splitlist.
+
+
+def split_string(source,splitlist):
+    split_words = []
+    index = 0
+    word = ''
+    for character in source:
+        if character in splitlist:
+            if word != '':
+                split_words.append(word)
+                word = ''
+        else:
+            word = word + character
+        index  = index + 1
+    if word != '':
+        split_words.append(word)
+    return split_words
+
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print(out)
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print(out)
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print(out)
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
+#Another way:
+def split_string(source,splitlist):
+    output = []
+    atsplit = True # At a split point
+    for char in source:   #iterate through string by each letter
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                #add char to last word
+                output[-1] = output[-1] + char
+    return output
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print(out)
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print(out)
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print(out)
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
+
+
+# The current index includes a url in the list of urls
+# for a keyword multiple times if the keyword appears
+# on that page more than once.
+
+# It might be better to only include the same url
+# once in the url list for a keyword, even if it appears
+# many times.
+
+# Modify add_to_index so that a given url is only
+# included once in the url list for a keyword,
+# no matter how many times that keyword appears.
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            if not url in entry[1]:
+                entry[1].append(url)
+            return
+    # not found, add new keyword to index
+    index.append([keyword, [url]])
+
+
+def get_page(url):
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return '''<html> <body> This is a test page for learning to crawl!
+<p> It is a good idea to
+<a href="http://www.udacity.com/cs101x/crawling.html">
+learn to crawl</a> before you try to
+<a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
+<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body>
+</html>'''
+
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return '''<html> <body> I have not learned to crawl yet, but I am
+quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
+</body> </html>'''
+
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return '''<html> <body> I cant get enough
+<a href="http://www.udacity.com/cs101x/index.html">crawling</a></body></html>'''
+
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return '''<html>
+<body>The magic words are Squeamish Ossifrage!</body></html>'''
+    except:
+        return ""
+    return ""
+
+def union(a, b):
+    for e in b:
+        if e not in a:
+            a.append(e)
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    index = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(tocrawl, get_all_links(content))
+            crawled.append(page)
+    return index
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return None
+
+index = crawl_web("http://www.udacity.com/cs101x/index.html")
+print(lookup(index,"is"))
+#>>> ['http://www.udacity.com/cs101x/index.html']
+
+
+# 2 Gold Stars
+
+# One way search engines rank pages
+# is to count the number of times a
+# searcher clicks on a returned link.
+# This indicates that the person doing
+# the query thought this was a useful
+# link for the query, so it should be
+# higher in the rankings next time.
+
+# (In Unit 6, we will look at a different
+# way of ranking pages that does not depend
+# on user clicks.)
+
+# Modify the index such that for each url in a
+# list for a keyword, there is also a number
+# that counts the number of times a user
+# clicks on that link for this keyword.
+
+# The result of lookup(index,keyword) should
+# now be a list of url entries, where each url
+# entry is a list of a url and a number
+# indicating the number of times that url
+# was clicked for this query keyword.
+
+# You should define a new procedure to simulate
+# user clicks for a given link:
+
+# record_user_click(index,word,url)
+
+# that modifies the entry in the index for
+# the input word by increasing the count associated
+# with the url by 1.
+
+# You also will have to modify add_to_index
+# in order to correctly create the new data
+# structure, and to prevent the repetition of
+# entries as in homework 4-5.
+
+
+def record_user_click(index,keyword,url):
+    urls = lookup(index, keyword)
+    if urls:
+        for entry in urls:
+            if entry[0] == url:
+                entry[1] == entry[1] + 1
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            for urls in entry[1]:
+                if urls[0] == url:
+                    return
+            entry[1].append([url,0])
+            return
+    # not found, add new keyword to index
+    index.append([keyword, [url,0]])
+
+
+def get_page(url):
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return '''<html> <body> This is a test page for learning to crawl!
+<p> It is a good idea to
+<a href="http://www.udacity.com/cs101x/crawling.html">
+learn to crawl</a> before you try to
+<a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
+<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body></html>'''
+
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return '''<html> <body> I have not learned to crawl yet, but I am
+quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
+</body> </html>'''
+
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return '''<html> <body> I cant get enough
+<a href="http://www.udacity.com/cs101x/index.html">crawling</a>!</body></html>'''
+
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return '<html><body>The magic words are Squeamish Ossifrage!</body></html>'
+    except:
+        return ""
+    return ""
+
+def union(a, b):
+    for e in b:
+        if e not in a:
+            a.append(e)
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    index = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(tocrawl, get_all_links(content))
+            crawled.append(page)
+    return index
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return None
+
+
+#Here is an example showing a sequence of interactions:
+index = crawl_web('http://www.udacity.com/cs101x/index.html')
+print(lookup(index, 'good'))
+#>>> [['http://www.udacity.com/cs101x/index.html', 0],
+#>>> ['http://www.udacity.com/cs101x/crawling.html', 0]]
+record_user_click(index, 'good', 'http://www.udacity.com/cs101x/crawling.html')
+print(lookup(index, 'good'))
+#>>> [['http://www.udacity.com/cs101x/index.html', 0],
+#>>> ['http://www.udacity.com/cs101x/crawling.html', 1]]
+
+
+# Write a procedure, count_words, which takes as input a string
+# and returns the number of words in the string. You may consider words
+# as strings of characters separated by spaces.
+
+def count_words(passage):
+    count = 0
+    for el in passage.split():
+        count += 1
+    return count
+  
+passage =("The number of orderings of the 52 cards in a deck of cards "
+"is so great that if every one of the almost 7 billion people alive "
+"today dealt one ordering of the cards per second, it would take "
+"2.5 * 10**40 times the age of the universe to order the cards in every "
+"possible way.")
+print(count_words(passage))
+#>>>56
+
+
+# Write a procedure, speed_fraction, which takes as its inputs the result of
+# a traceroute (in ms) and distance (in km) between two points. It should 
+# return the speed the data travels as a decimal fraction of the speed of
+# light.
+
+speed_of_light = 300000. # km per second
+
+def speed_fraction(traceroute,distance):
+    return (2. * distance / traceroute * 1000.) / speed_of_light
+
+
+
+print(speed_fraction(50,5000))
+#>>> 0.666666666667
+
+print(speed_fraction(50,10000))
+#>>> 1.33333333333  # Any thoughts about this answer, or these inputs?
